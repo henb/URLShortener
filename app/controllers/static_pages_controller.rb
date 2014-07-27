4205b53d2ac2df2a8202d5508ade4a4eb1c5f637
henb <henb2015@gmail.com>
2014-07-27 13:07:19 +0300

    Refactoring with respond_with

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,19 +1,16 @@
 class StaticPagesController < ApplicationController
+  respond_to :html, :js, only: :random
+
   def home
-  	@url = Url.new
+    @url = Url.new
   end
 
   def api
   end
 
   def random
-    @url = Url.where(:public=>true).sample
+    @url = Url.where(public: true).sample
 
-    respond_to do |format|
-      format.js
-      format.json
-      format.html
-    end
+    respond_with
   end
-
 end

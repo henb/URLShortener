2e960055276bdd2bc6be3b349c1d96c56ade2f00
henb <henb2015@gmail.com>
2014-07-30 21:19:36 +0300

    Review snake_url

--- app/models/url.rb
+++ app/models/url.rb
@@ -6,4 +6,18 @@ class Url < ActiveRecord::Base
   scope :published, -> { where(public: true) }
   scope :pagination, ->(page, limit) { paginate(page: page, per_page: limit || 10) }
   scope :url_alias, ->(id) { find_by(alias: id) }
+
+  before_save :snake_url
+
+  def snake_url
+    self.alias.strip!
+    return nil if self.alias.empty?
+    return self.alias = 'lomai_dalwe' if self.alias.index(/urls|api|random/)
+    self.alias.to_s.gsub!(/\W/, '_')
+
+    if self.alias.count('a-zA-Z0-9') > 0
+      self.alias = self.alias[0..self.alias.rindex(/[a-zA-Z0-9]/)]
+      self.alias = self.alias[self.alias.index(/[a-zA-Z0-9]/)..-1]
+    end
+  end
 end

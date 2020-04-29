ce4f5840e6ee8aa37f14ee906b591a45c3c2856b
henb <henb2015@gmail.com>
2020-04-29 19:02:31 +0300

    Update url.rb

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,7 +1,7 @@
 class Url < ActiveRecord::Base
   validates :alias, uniqueness: true, presence: true, length: { in: 1..100 }
   validates :link, presence: true, length: { maximum: 10_000 }
-  validates :public, presence: true
+  validates :public
 
   scope :published, -> { where(public: true) }
   scope :pagination, ->(page, limit) { paginate(page: page, per_page: limit || 10) }
